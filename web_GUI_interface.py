9548ce6898a0b1e58999086d5932aca00c0f0506
rocketpy <dimitr.ushakov@gmail.com>
2021-06-06 17:18:50 +0300

    Update web_GUI_interface.py

--- web_GUI_interface.py
+++ web_GUI_interface.py
@@ -1,1 +1,7 @@
+#  Eel  - For little HTML GUI applications, with easy Python/JS interop
 
+# PyPi: https://pypi.org/project/Eel/
+# Eel examples: https://github.com/ChrisKnott/Eel/tree/master/examples
+
+
+# pip install Eel
